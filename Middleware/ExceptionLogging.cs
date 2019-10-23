1f6a7596431d4d98f928d5cb5d4740a3fa081844
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:56:47 -0700

    Framework extraction update
    Framework extraction update

---
+++ Middleware/ExceptionLogging.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Penguin.Cms.Errors;
+using Penguin.Cms.Errors.Extensions;
+using Penguin.Persistence.Abstractions.Interfaces;
+using Penguin.Security.Abstractions.Interfaces;
+using Penguin.Web.Abstractions.Interfaces;
+using System;
+
+using System.Threading.Tasks;
+
+namespace Penguin.Cms.Web.Errors.Middleware
+{
+    //https://exceptionnotfound.net/using-middleware-to-log-requests-and-responses-in-asp-net-core/
+    public class ExceptionLogging : IPenguinMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        //TODO: Learn what this is
+        public ExceptionLogging(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await this._next(context).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+                IRepository<AuditableError> errorRepository = context.RequestServices.GetService<IRepository<AuditableError>>();
+
+                errorRepository.TryAdd(ex, false, context.Request.Path.Value, context.RequestServices.GetService<IUserSession>()?.LoggedInUser?.Guid);
+
+                throw;
+            }
+        }
+    }
+}
